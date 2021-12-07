ecbf66917e4eb3f5b17242e6b670f019da54cc9f
Petrashka <serj42@yandex.ru>
2021-12-07 12:28:19 +0300

    added sort of  keys and namespaces

--- src/components/TranslatesGrid/index.js
+++ src/components/TranslatesGrid/index.js
@@ -1,103 +1,151 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { lighten, makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
-import IconButton from "@material-ui/core/IconButton";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { lighten, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
 
-import Checkbox from "@material-ui/core/Checkbox";
-import Tooltip from "@material-ui/core/Tooltip";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Switch from "@material-ui/core/Switch";
-import DeleteIcon from "@material-ui/icons/Delete";
-import EditIcon from "@material-ui/icons/Edit";
-import FilterListIcon from "@material-ui/icons/FilterList";
+import Checkbox from '@material-ui/core/Checkbox';
+import Tooltip from '@material-ui/core/Tooltip';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import FilterListIcon from '@material-ui/icons/FilterList';
 
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles({
-  table: {
-    minWidth: 700,
-  },
+    table: {
+        minWidth: 700,
+    },
 });
 
 const TranslatesGrid = ({ data, history, onDelete, dense = true }) => {
-  const { t } = useTranslation();
-  const classes = useStyles();
-  return (
-    <TableContainer component={Paper}>
-      <Table
-        size={dense ? "small" : "medium"}
-        stickyHeader
-        className={classes.table}
-        aria-label="spanning table"
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">{t("tableheader.key")}</TableCell>
-            <TableCell align="center">{t("tableheader.namespace")}</TableCell>
-            <TableCell align="center">{t("tableheader.langs")}</TableCell>
-            <TableCell align="center">{t("tableheader.edit/delete")}</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Array.isArray(data)
-            ? data.map((row) => (
-                <TableRow key={row.value}>
-                  <TableCell align="center">{row.key}</TableCell>
-                  <TableCell align="center">{row.namespace || ""}</TableCell>
-                  <TableCell align="center"></TableCell>
-                  <TableCell align="center">
-                    <IconButton
-                      color="primary"
-                      onClick={() => {
-                        history.push(
-                          `/translates/edit?${new URLSearchParams({
-                            key: row.key,
-                            namespace: row.namespace,
-                          }).toString()}`
-                        );
-                      }}
-                    >
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton
-                      color="secondary"
-                      onClick={() =>
-                        onDelete({ key: row.key, namespace: row.namespace })
-                      }
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              ))
-            : null}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    const { t } = useTranslation();
+    const classes = useStyles();
+    const [sort, setSort] = React.useState({
+        field: 'key',
+        direct: 1,
+    });
+
+    const res = React.useMemo(() => {
+        return data.sort((a, b) => {
+            console.log(a[sort.field][0] - b[sort.field][0]);
+            return sort.direct > 0
+                ? a[sort.field][0] > b[sort.field][0]
+                : a[sort.field][0] < b[sort.field][0];
+        });
+    }, [sort]);
+
+    return (
+        <TableContainer component={Paper}>
+            <Table
+                size={dense ? 'small' : 'medium'}
+                stickyHeader
+                className={classes.table}
+                aria-label="spanning table"
+            >
+                <TableHead>
+                    <TableRow>
+                        <TableCell
+                            onClick={() => {
+                                setSort({
+                                    field: 'key',
+                                    direct: sort.direct * -1,
+                                });
+                            }}
+                            align="center"
+                        >
+                            {t('tableheader.key')}
+                        </TableCell>
+                        <TableCell
+                            onClick={() => {
+                                setSort({
+                                    field: 'namespace',
+                                    direct: sort.direct * -1,
+                                });
+                            }}
+                            align="center"
+                        >
+                            {t('tableheader.namespace')}
+                        </TableCell>
+                        <TableCell align="center">
+                            {t('tableheader.langs')}
+                        </TableCell>
+                        <TableCell align="center">
+                            {t('tableheader.edit/delete')}
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {Array.isArray(res)
+                        ? res.map((row) => (
+                              <TableRow key={row.value}>
+                                  <TableCell align="center">
+                                      {row.key}
+                                  </TableCell>
+                                  <TableCell align="center">
+                                      {row.namespace || ''}
+                                  </TableCell>
+                                  <TableCell align="center"></TableCell>
+                                  <TableCell align="center">
+                                      <IconButton
+                                          color="primary"
+                                          onClick={() => {
+                                              history.push(
+                                                  `/translates/edit?${new URLSearchParams(
+                                                      {
+                                                          key: row.key,
+                                                          namespace:
+                                                              row.namespace,
+                                                      },
+                                                  ).toString()}`,
+                                              );
+                                          }}
+                                      >
+                                          <EditIcon />
+                                      </IconButton>
+                                      <IconButton
+                                          color="secondary"
+                                          onClick={() =>
+                                              onDelete({
+                                                  key: row.key,
+                                                  namespace: row.namespace,
+                                              })
+                                          }
+                                      >
+                                          <DeleteIcon />
+                                      </IconButton>
+                                  </TableCell>
+                              </TableRow>
+                          ))
+                        : null}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 };
 
 TranslatesGrid.propTypes = {
-  data: PropTypes.array.isRequired,
-  history: PropTypes.object,
-  onDelete: PropTypes.func.isRequired,
-  dense: PropTypes.bool,
+    data: PropTypes.array.isRequired,
+    history: PropTypes.object,
+    onDelete: PropTypes.func.isRequired,
+    dense: PropTypes.bool,
 
-  // : PropTypes.
+    // : PropTypes.
 };
 TranslatesGrid.defaultProps = {
-  data: [],
+    data: [],
 };
 
 export default TranslatesGrid;

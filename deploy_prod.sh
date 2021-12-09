73ff1d10840c380b6e93593ebb048870ba52adbf
Petrashka <serj42@yandex.ru>
2021-12-09 15:04:51 +0300

    added deploy  sh

---
+++ deploy_prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# node ./prepare/sitemap/index.js
+# node ./prepare/robot.js
+sudo sshfs -o allow_other,default_permissions,IdentityFile=/home/serj/.ssh/id_rsa user@46.101.168.74:/ /mnt/prod
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+VAR1="/build/*"
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )${VAR1}"
+
+cp -r $SCRIPTPATH  /mnt/prod/home/user/project/translates_front
+echo -ne '#######################   (100%)\r'
+sudo fusermount -u  /mnt/prod
+echo -ne '\n'

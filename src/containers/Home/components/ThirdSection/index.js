7b8e68aa114b0b71a6fc191e505fe18264e13f83
siargei petraskha <piotrazsko@gmail.com>
2023-01-02 11:45:27 +0100

    added mobile images

--- src/containers/Home/components/ThirdSection/index.js
+++ src/containers/Home/components/ThirdSection/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 
 import image from 'assets/images/landing/way.png';
 import imageWebp from 'assets/images/landing/way.webp';
 import imageAvif from 'assets/images/landing/way.avif';
+import imageAvifMobile from 'assets/images/landing/way_mobile.avif';
+import imagePNGMobile from 'assets/images/landing/way_mobile.png';
+import imageWebPMobile from 'assets/images/landing/way_mobile.webp';
 
 import Section from '../Section';
 import style from './style.scss';
@@ -24,6 +25,21 @@ export const ThirdSection = ({ t }) => {
                         possible
                     </Typography>
                     <picture className={style.image}>
+                        <source
+                            media="(max-width:650px)"
+                            srcSet={imageAvifMobile}
+                            type="image/avif"
+                        ></source>
+                        <source
+                            media="(max-width:650px)"
+                            srcSet={imageWebPMobile}
+                            type="image/webp"
+                        ></source>
+                        <source
+                            media="(max-width:650px)"
+                            srcSet={imagePNGMobile}
+                            type="image/png"
+                        ></source>
                         <source srcSet={imageAvif} type="image/avif"></source>
                         <source srcSet={imageWebp} type="image/webp"></source>
                         <img
